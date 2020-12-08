5fd9734540ad4228ca0622234f38eb172bfdabc7
sonamk15 <sonamk18@navgurukul.org>
2020-12-08 19:40:45 +0530

    authentication and swagger implementation

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const bodyParser  = require('body-parser');
 const morgan = require('morgan');
 const port =  5000;
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(morgan('dev'));
+
+app.use('/swagger-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
 app.use('/api', require('./lib/routes/users').router);
 
 app.listen(port, () =>
